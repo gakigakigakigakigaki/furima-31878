1bc43701fc8f52b8ad9ed08db7b1000aacf86454
gakigakigakigakigaki <73731407+gakigakigakigakigaki@users.noreply.github.com>
2020-11-25 09:37:58 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validetes :email,    presence: true
+  validetes :encrypted_password, presence: true
+  validetes :first_name, presence: true
+  validetes :last_name, presence: true
+  validetes :first_name_kana, presence: true
+  validetes :last_name_kana, presence: true
 end
