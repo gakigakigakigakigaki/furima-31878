d89162f47a0d1c1f0a2516c6745ea36806ae5a04
gakigakigakigakigaki <73731407+gakigakigakigakigaki@users.noreply.github.com>
2020-11-26 20:37:00 +0900

    商品の状態作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
 
   validates :title, text, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
+  validates :status_id numericality: { other_than: 1}
 end
