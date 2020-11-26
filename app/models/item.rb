d04dace5637322787d103a9e7896507ff696563d
gakigakigakigakigaki <73731407+gakigakigakigakigaki@users.noreply.github.com>
2020-11-26 20:13:56 +0900

    ActiveHashを導入·ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :title, text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
 end
