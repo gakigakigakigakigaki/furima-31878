3536212d632f214217c3f8be8a1e9be9e8c75b75
gakigakigakigakigaki <73731407+gakigakigakigakigaki@users.noreply.github.com>
2020-11-26 20:44:44 +0900

    配送料の負担

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery_fee
 
   validates :title, text, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id numericality: { other_than: 1}
+  validates :delivery_fee_id, numericality: { other_than: 1 }
 end
