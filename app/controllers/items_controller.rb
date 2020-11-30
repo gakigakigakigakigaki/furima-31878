25da69fbc0ddb5af159e034cf6f61be269c6b0d6
gakigakigakigakigaki <73731407+gakigakigakigakigaki@users.noreply.github.com>
2020-11-30 16:56:06 +0900

    ログイン·ログアウト時の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -22,7 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end  
+
 private
+def move_to_index
+  item = Item.find(params[:id])
+  if item.user_id != current_user.id
+    redirect_to action: :index
+  end
+end
+
   def item_params
     params.require(:item).permit(:product_name, :price, :category_id, :status_id, :description, :prefecture_id, :days_to_ship_id, :delivery_fee_id,:image).merge( user_id: current_user.id)
   end
